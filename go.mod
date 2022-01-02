4e741d75f4461e7373ec7819d11452edf77b5f2b
Sybren A. Stüvel <sybren@stuvel.eu>
2022-01-02 11:32:09 +0100

    Correct logging colors on Windows

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.16
 
 require (
 	github.com/dop251/goja v0.0.0-20211217115348-3f9136fa235d
+	github.com/mattn/go-colorable v0.1.12
 	github.com/rs/zerolog v1.26.1
-	golang.org/dl v0.0.0-20211214194107-65ed43b8dfff // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"os"
 	"time"
 
+	"github.com/mattn/go-colorable"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"stuvel.eu/flamenco-test/goja/job_compilers"
 )
 
 func main() {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	output := zerolog.ConsoleWriter{Out: colorable.NewColorableStdout(), TimeFormat: time.RFC3339}
 	log.Logger = log.Output(output)
 
 	compiler, err := job_compilers.Load()
