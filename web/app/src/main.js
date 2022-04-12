77cc0f36e6a70ad0df20ea4be6197f3976c5cf37
Sybren A. Stüvel <sybren@blender.org>
2022-04-12 16:10:51 +0200

    Web: remove API example code, enough examples in the code now

--- web/app/src/main.js
+++ web/app/src/main.js
@@ -8,24 +8,6 @@ import router from './router'
 import { DateTime } from 'luxon';
 window.DateTime = DateTime;
 
-// // Example of how to use the Flamenco Manager API:
-// import * as urls from './urls'
-// import {
-//   ApiClient,
-//   MetaApi,
-// } from './manager-api'
-
-// let apiClient = new ApiClient(urls.api());
-// console.log('ApiClient:', apiClient);
-// const metaAPI = new MetaApi(apiClient);
-// console.log("metaAPI:", metaAPI);
-// metaAPI.getVersion().then(function (version) {
-//   console.log("version:", version);
-// })
-// metaAPI.getConfiguration().then(function (config) {
-//   console.log("config:", config);
-// })
-
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
