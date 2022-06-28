37d8fbdf5e8a7ed76325989de2057efff09d51be
Sybren A. Stüvel <sybren@blender.org>
2022-06-28 12:32:09 +0200

    Web: remove 'settings' view
    It's not in use, and better to not have it at all then.

--- web/app/src/router/index.js
+++ web/app/src/router/index.js
@@ -20,11 +20,6 @@ const router = createRouter({
       component: () => import('../views/WorkersView.vue'),
       props: true,
     },
-    {
-      path: '/settings',
-      name: 'settings',
-      component: () => import('../views/SettingsView.vue')
-    },
   ],
 })
 
