cc56e5b22e27921758b8816b93ab78a61d25e673
Sybren A. Stüvel <sybren@blender.org>
2022-06-28 17:15:27 +0200

    Web: auto-detect whether the port number should be overridden
    It's a bit clumsy (if the web port is :8081, override it to :8080 for the
    API requests), but works in our case.

--- web/app/src/urls.js
+++ web/app/src/urls.js
@@ -2,7 +2,9 @@
 let url = new URL(window.location.href);
 // Uncomment this when the web interface is running on a different port than the
 // API, for example when using the Vite devserver. Set the API port here.
-// url.port = "8080";
+if (url.port == "8081") {
+  url.port = "8080";
+}
 url.pathname = "/";
 const flamencoAPIURL = url.href;
 
