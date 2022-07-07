204d9d1156f6367dcf46a777612d329dc928c772
Pablo Vazquez <pablo@blender.org>
2022-07-07 17:48:23 +0200

    Footer: Add arrow icon to indicate that can be expanded

--- web/app/src/views/JobsView.vue
+++ web/app/src/views/JobsView.vue
@@ -12,6 +12,11 @@
 
   <footer class="app-footer" v-if="!showFooterPopup" @click="showFooterPopup = true">
     <notification-bar />
+    <div class="app-footer-expand">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <line x1="12" y1="19" x2="12" y2="5"></line><polyline points="5 12 12 5 19 12"></polyline>
+      </svg>
+    </div>
   </footer>
   <footer-popup v-if="showFooterPopup" ref="footerPopup" @clickClose="showFooterPopup = false" />
 
