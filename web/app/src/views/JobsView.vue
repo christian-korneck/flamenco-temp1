ea1bbdf95a65c5abbac81c262ac26a2ecf833583
Sybren A. Stüvel <sybren@blender.org>
2022-08-02 10:41:05 +0200

    Cleanup: web, reformat Vue component
    Just applying the auto-formatting.

--- web/app/src/views/JobsView.vue
+++ web/app/src/views/JobsView.vue
@@ -13,19 +13,19 @@
   <footer class="app-footer" v-if="!showFooterPopup" @click="showFooterPopup = true">
     <notification-bar />
     <div class="app-footer-expand">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <line x1="12" y1="19" x2="12" y2="5"></line><polyline points="5 12 12 5 19 12"></polyline>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="2"
+        stroke-linecap="round" stroke-linejoin="round">
+        <line x1="12" y1="19" x2="12" y2="5"></line>
+        <polyline points="5 12 12 5 19 12"></polyline>
       </svg>
     </div>
   </footer>
   <footer-popup v-if="showFooterPopup" ref="footerPopup" @clickClose="showFooterPopup = false" />
 
-  <update-listener ref="updateListener"  mainSubscription="allJobs"
-    :subscribedJobID="jobID" :subscribedTaskID="taskID"
+  <update-listener ref="updateListener" mainSubscription="allJobs" :subscribedJobID="jobID" :subscribedTaskID="taskID"
     @jobUpdate="onSioJobUpdate" @taskUpdate="onSioTaskUpdate" @taskLogUpdate="onSioTaskLogUpdate"
-    @lastRenderedUpdate="onSioLastRenderedUpdate"
-    @message="onChatMessage"
-    @sioReconnected="onSIOReconnected" @sioDisconnected="onSIODisconnected" />
+    @lastRenderedUpdate="onSioLastRenderedUpdate" @message="onChatMessage" @sioReconnected="onSIOReconnected"
+    @sioDisconnected="onSIODisconnected" />
 </template>
 
 <script>
