2185d8b5eed48e44708de01f0a4706cd10dc22e5
Sybren A. Stüvel <sybren@blender.org>
2022-08-02 10:41:47 +0200

    Web: only show the task details when there is a job
    The "select a task" text only makes sense when there is a task to be
    selected, so if there is no selected job, don't even show that.

--- web/app/src/views/JobsView.vue
+++ web/app/src/views/JobsView.vue
@@ -7,7 +7,7 @@
     <tasks-table v-if="hasJobData" ref="tasksTable" :jobID="jobID" :taskID="taskID" @tableRowClicked="onTableTaskClicked" />
   </div>
   <div class="col col-3">
-    <task-details :taskData="tasks.activeTask" @showTaskLogTail="showTaskLogTail" />
+    <task-details v-if="hasJobData" :taskData="tasks.activeTask" @showTaskLogTail="showTaskLogTail" />
   </div>
 
   <footer class="app-footer" v-if="!showFooterPopup" @click="showFooterPopup = true">
