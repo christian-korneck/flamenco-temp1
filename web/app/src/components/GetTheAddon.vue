2d6475a7e1c0d7eb3fc468926527bc009ae3c747
Sybren A. Stüvel <sybren@blender.org>
2022-08-02 11:04:11 +0200

    Web: make the "get the add-on" button actually do something
    The button is now a link that leads to the add-on ZIP.

--- web/app/src/components/GetTheAddon.vue
+++ web/app/src/components/GetTheAddon.vue
@@ -2,7 +2,7 @@
   <div class="details-no-item-selected">
     <div class="get-the-addon">
       <p>Get the Blender add-on and submit a job.</p>
-      <p><button class="btn btn-primary">Get the add-on!</button></p>
+      <p><a class="btn btn-primary" :href="backendURL('/flamenco3-addon.zip')">Get the add-on!</a></p>
       <p>Use the URL below in the add-on preferences. Click on it to copy.</p>
       <p><span class="click-to-copy" title="Click to copy this URL" @click="copyElementText">{{ api() }}</span></p>
     </div>
@@ -10,7 +10,7 @@
 </template>
 
 <script setup>
-import { api } from '@/urls';
+import { api, backendURL } from '@/urls';
 import { copyElementText } from '@/clipboard.js';
 </script>
 
